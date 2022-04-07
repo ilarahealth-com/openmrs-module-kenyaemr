4dfc288f40bde913b1a863deacc899c09e4f94d6
nathankorir <nathan.korir@ilarahealth.com>
2022-04-07 16:36:00 +0300

    Add place of delivery to maternity register

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/maternity/MaternityPlaceOfDeliveryDataDefinition.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/maternity/MaternityPlaceOfDeliveryDataDefinition.java
@@ -1,2 +1,38 @@
-package org.openmrs.module.kenyaemr.reporting.data.converter.definition.maternity;public class MaternityPlaceOfDeliveryDataDefinition {
+package org.openmrs.module.kenyaemr.reporting.data.converter.definition.maternity;
+
+import org.openmrs.module.reporting.data.BaseDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationPropertyCachingStrategy;
+import org.openmrs.module.reporting.evaluation.caching.Caching;
+
+/**
+ * Maternity Place of Delivery Column
+ */
+@Caching(strategy= ConfigurationPropertyCachingStrategy.class)
+public class MaternityPlaceOfDeliveryDataDefinition extends BaseDataDefinition implements PersonDataDefinition {
+
+    public static final long serialVersionUID = 1L;
+
+    /**
+     * Default Constructor
+     */
+    public MaternityPlaceOfDeliveryDataDefinition() {
+        super();
+    }
+
+    /**
+     * Constructor to populate name only
+     */
+    public MaternityPlaceOfDeliveryDataDefinition(String name) {
+        super(name);
+    }
+
+    //***** INSTANCE METHODS *****
+
+    /**
+     * @see org.openmrs.module.reporting.data.DataDefinition#getDataType()
+     */
+    public Class<?> getDataType() {
+        return Double.class;
+    }
 }
