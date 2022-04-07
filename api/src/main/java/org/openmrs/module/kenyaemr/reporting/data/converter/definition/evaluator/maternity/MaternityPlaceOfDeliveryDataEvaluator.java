df8a1c40f554b8a2667f1bce6a0fbdd25ec81572
nathankorir <nathan.korir@ilarahealth.com>
2022-04-07 17:07:58 +0300

    Add data evaluator

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/maternity/MaternityPlaceOfDeliveryDataEvaluator.java
@@ -0,0 +1,38 @@
+package org.openmrs.module.kenyaemr.reporting.data.converter.definition.evaluator.maternity;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.maternity.MaternityPlaceOfDeliveryDataDefinition;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+
+import java.util.Map;
+
+/**
+ * Evaluates a PersonDataDefinition
+ */
+@Handler(supports= MaternityPlaceOfDeliveryDataDefinition.class, order=50)
+public class MaternityPlaceOfDeliveryDataEvaluator implements PersonDataEvaluator {
+
+    @Autowired
+    private EvaluationService evaluationService;
+
+    public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context) throws EvaluationException {
+        EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+
+        String qry = "select\n" +
+                "  patient_id,\n" +
+                "  (case place_of_delivery when 1536 then \"Home\" when 1589 then \"Facility\" when 1601 then \"Born Before Arrival\" else \"\" end) as place_of_delivery\n" +
+                "from kenyaemr_etl.etl_mchs_delivery;";
+
+        SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+        queryBuilder.append(qry);
+        Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+        c.setData(data);
+        return c;
+    }
+}
