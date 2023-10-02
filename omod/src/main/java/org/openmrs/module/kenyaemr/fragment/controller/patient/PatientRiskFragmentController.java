c5d1fd83dcd7fc1c4ebb15e77971e6cf301d3607
nathankorir <nathan.korir@ilarahealth.com>
2023-10-02 13:53:46 +0300

    Add logic for request to Mihic

--- omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/patient/PatientRiskFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/patient/PatientRiskFragmentController.java
@@ -3,7 +3,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
  * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
+ * <p>
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
@@ -42,118 +42,147 @@ import java.util.Set;
  */
 public class PatientRiskFragmentController {
 
-	protected static final Log log = LogFactory.getLog(PatientRiskFragmentController.class);
+    protected static final Log log = LogFactory.getLog(PatientRiskFragmentController.class);
 
-	ObsService obsService = Context.getObsService();
-	ConceptService conceptService = Context.getConceptService();
-	SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy");
+    ObsService obsService = Context.getObsService();
+    ConceptService conceptService = Context.getConceptService();
+    SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy");
 
-	// triage concepts
-	Concept HAEMOGLOBIN = Dictionary.getConcept("21AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	Concept BP_SYSTOLIC = Dictionary.getConcept("5085AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	Concept BP_DIASTOLIC = Dictionary.getConcept("5086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	Concept PALLOR = Dictionary.getConcept("5245AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	Concept PULSE_RATE = Dictionary.getConcept("5087AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	Concept ANAEMIA = Dictionary.getConcept("121629AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-	Concept ESTIMATED_DELIVERY_DATE = Dictionary.getConcept("5596AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    // triage concepts
+    Concept HAEMOGLOBIN = Dictionary.getConcept("21AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    Concept BP_SYSTOLIC = Dictionary.getConcept("5085AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    Concept BP_DIASTOLIC = Dictionary.getConcept("5086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    Concept PALLOR = Dictionary.getConcept("5245AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    Concept PULSE_RATE = Dictionary.getConcept("5087AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    Concept ANAEMIA = Dictionary.getConcept("121629AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    Concept ESTIMATED_DELIVERY_DATE = Dictionary.getConcept("5596AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    Concept FOETAL_HEART_RATE = Dictionary.getConcept("1440AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    Concept ANC_VISIT_NUMBER = Dictionary.getConcept("1425AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
-	public void controller(@FragmentParam("patient") Patient patient, FragmentModel model) {
-		List<Obs> obs = null
-		Obs haemoglobin = getLatestObs(patient, HAEMOGLOBIN)
-		if (haemoglobin != null) {
-			obs.add(haemoglobin)
-		}
-		Obs bp_systolic = getLatestObs(patient, BP_SYSTOLIC)
-		if (bp_systolic != null) {
-			obs.add(bp_systolic)
-		}
-		Obs bp_diastolic = getLatestObs(patient, BP_DIASTOLIC)
-		if (bp_diastolic != null) {
-			obs.add(bp_diastolic)
-		}
-		Obs pallor = getLatestObs(patient, PALLOR)
-		if (pallor != null) {
-			obs.add(pallor)
-		}
-		Obs pulse = getLatestObs(patient, PULSE_RATE)
-		if (pulse != null) {
-			obs.add(pulse)
-		}
-		Obs anaemia = getLatestObs(patient, ANAEMIA)
-		if (anaemia != null) {
-			obs.add(anaemia)
-		}
-		Obs edd = getLatestObs(patient, ESTIMATED_DELIVERY_DATE)
-		if (edd != null) {
-			obs.add(edd)
-		}
-	}
+    public void controller(@FragmentParam("patient") Patient patient, FragmentModel model) {
+        List<Obs> obs = null
+        Obs haemoglobin = getLatestObs(patient, HAEMOGLOBIN)
+        if (haemoglobin != null) {
+            obs.add(haemoglobin)
+        }
+        Obs bp_systolic = getLatestObs(patient, BP_SYSTOLIC)
+        if (bp_systolic != null) {
+            obs.add(bp_systolic)
+        }
+        Obs bp_diastolic = getLatestObs(patient, BP_DIASTOLIC)
+        if (bp_diastolic != null) {
+            obs.add(bp_diastolic)
+        }
+        Obs pallor = getLatestObs(patient, PALLOR)
+        if (pallor != null) {
+            obs.add(pallor)
+        }
+        Obs pulse = getLatestObs(patient, PULSE_RATE)
+        if (pulse != null) {
+            obs.add(pulse)
+        }
+        Obs anaemia = getLatestObs(patient, ANAEMIA)
+        if (anaemia != null) {
+            obs.add(anaemia)
+        }
+        Obs edd = getLatestObs(patient, ESTIMATED_DELIVERY_DATE)
+        if (edd != null) {
+            obs.add(edd)
+        }
+        Obs fhr = getLatestObs(patient, FOETAL_HEART_RATE)
+        if (fhr != null) {
+            obs.add(fhr)
+        }
+        Obs visitNumber = getLatestObs(patient, ANC_VISIT_NUMBER)
+        if (visitNumber != null) {
+            obs.add(visitNumber)
+        }
+        model.addAttribute("riskParams", getRiskParams(obs));
+    }
 
-	private SimpleObject getRiskParams(List<Obs> obsList) {
-		Double haemoglobin = null;
+    private SimpleObject getRiskParams(List<Obs> obsList) {
+        Double haemoglobin = null;
 
-		Map<String, Object> riskParamsMap = new HashMap<String, Object>();
-		for (Obs obs : obsList) {
-			if (obs.getConcept().equals(HAEMOGLOBIN) ) {
-				haemoglobin = obs.getValueNumeric();
-				if(!riskParamsMap.keySet().contains("haemoglobin")) {
-					riskParamsMap.put("haemoglobin", haemoglobin);
-				}
-			} else if (obs.getConcept().equals(PALLOR) ) {
-				pallor = obs.getValueCoded();
-				if(!riskParamsMap.keySet().contains("pallor")) {
-					riskParamsMap.put("pallor", pallor);
-				}
-			} else if (obs.getConcept().equals(PULSE_RATE) ) {
-				pulse = obs.getValueNumeric();
-				if(!riskParamsMap.keySet().contains("pulse")) {
-					riskParamsMap.put("pulse", pallor);
-				}
-			} else if (obs.getConcept().equals(ANAEMIA) ) {
-				anaemia = obs.getValueCoded();
-				if(!riskParamsMap.keySet().contains("anaemia")) {
-					riskParamsMap.put("anaemia", pallor);
-				}
-			}else if (obs.getConcept().equals(BP_SYSTOLIC )) {
-				bp_systolic = obs.getValueNumeric();
-				if(!riskParamsMap.keySet().contains("bp_systolic")) {
-					riskParamsMap.put("bp_systolic", bp_systolic.intValue());
-				}
-			} else if (obs.getConcept().equals(BP_DIASTOLIC )) {
-				bp_diastolic = obs.getValueNumeric();
-				if(!riskParamsMap.keySet().contains("bp_diastolic")) {
-					riskParamsMap.put("bp_diastolic", bp_diastolic.intValue());
-				}
-			}else if (obs.getConcept().equals(ESTIMATED_DELIVERY_DATE )) {
-				edd = DATE_FORMAT.format(obs.getValueDate());
-				if(!riskParamsMap.keySet().contains("edd")) {
-					riskParamsMap.put("edd", edd);
-				}
-			}
+        Map<String, Object> riskParamsMap = new HashMap<String, Object>();
+        for (Obs obs : obsList) {
+            if (obs.getConcept().equals(HAEMOGLOBIN)) {
+                haemoglobin = obs.getValueNumeric();
+                if (!riskParamsMap.keySet().contains("haemoglobin")) {
+                    riskParamsMap.put("haemoglobin", haemoglobin);
+                }
+            } else if (obs.getConcept().equals(PALLOR)) {
+                pallor = obs.getValueCoded();
+                if (!riskParamsMap.keySet().contains("pallor")) {
+                    riskParamsMap.put("pallor", pallor);
+                }
+            } else if (obs.getConcept().equals(PULSE_RATE)) {
+                pulse = obs.getValueNumeric();
+                if (!riskParamsMap.keySet().contains("pulse")) {
+                    riskParamsMap.put("pulse", pallor);
+                }
+            } else if (obs.getConcept().equals(ANAEMIA)) {
+                anaemia = obs.getValueCoded();
+                if (!riskParamsMap.keySet().contains("anaemia")) {
+                    riskParamsMap.put("anaemia", pallor);
+                }
+            } else if (obs.getConcept().equals(BP_SYSTOLIC)) {
+                bp_systolic = obs.getValueNumeric();
+                if (!riskParamsMap.keySet().contains("bp_systolic")) {
+                    riskParamsMap.put("bp_systolic", bp_systolic.intValue());
+                }
+            } else if (obs.getConcept().equals(BP_DIASTOLIC)) {
+                bp_diastolic = obs.getValueNumeric();
+                if (!riskParamsMap.keySet().contains("bp_diastolic")) {
+                    riskParamsMap.put("bp_diastolic", bp_diastolic.intValue());
+                }
+            } else if (obs.getConcept().equals(ESTIMATED_DELIVERY_DATE)) {
+                edd = DATE_FORMAT.format(obs.getValueDate());
+                if (!riskParamsMap.keySet().contains("edd")) {
+                    riskParamsdfsMap.put("edd", edd);
+                }
+            } else if (obs.getConcept().equals(FOETAL_HEART_RATE)) {
+                fhr = obs.getValueNumeric();
+                if (!riskParamsMap.keySet().contains("fhr")) {
+                    riskParamsdfsMap.put("fhr", fhr.intValue());
+                }
+            }
+            else if (obs.getConcept().equals(ANC_VISIT_NUMBER)) {
+                visitNumber = obs.getValueNumeric();
+                if (!riskParamsMap.keySet().contains("visitNumber")) {
+                    riskParamsdfsMap.put("visitNumber", visitNumber.intValue());
+                }
+                visitDate = DATE_FORMAT.format(obs.getDateCreated());
+                if (!riskParamsMap.keySet().contains("visitDate")) {
+                    riskParamsdfsMap.put("visitDate", visitDate);
+                }
+            }
 
-		}
+        }
 
-		if (bp_diastolic != null && bp_systolic != null)
-			riskParamsMap.put("bp", new StringBuilder().append(bp_systolic.intValue()).append("/").append(bp_diastolic.intValue()));
+        if (bp_diastolic != null && bp_systolic != null)
+            riskParamsMap.put("bp", new StringBuilder().append(bp_systolic.intValue()).append("/").append(bp_diastolic.intValue()));
 
 
-		return SimpleObject.create(
-				"haemoglobin", riskParamsMap.get("haemoglobin") != null? new StringBuilder().append(riskParamsMap.get("haemoglobin")),
-				"pallor", riskParamsMap.get("pallor") != null? new StringBuilder().append(riskParamsMap.get("pallor")),
-				"pulse", riskParamsMap.get("pulse") != null? new StringBuilder().append(riskParamsMap.get("pulse")),
-				"anaemia", riskParamsMap.get("anaemia") != null? new StringBuilder().append(riskParamsMap.get("anaemia")),
-				"edd", riskParamsMap.get("edd") != null? new StringBuilder().append(riskParamsMap.get("edd")),
-				"bp", riskParamsMap.get("bp") != null? new StringBuilder().append(riskParamsMap.get("bp"))
-		);
-	}
+        return SimpleObject.create(
+                "haemoglobin", riskParamsMap.get("haemoglobin") != null ? new StringBuilder().append(riskParamsMap.get("haemoglobin")),
+                "pallor", riskParamsMap.get("pallor") != null ? new StringBuilder().append(riskParamsMap.get("pallor")),
+                "pulse", riskParamsMap.get("pulse") != null ? new StringBuilder().append(riskParamsMap.get("pulse")),
+                "anaemia", riskParamsMap.get("anaemia") != null ? new StringBuilder().append(riskParamsMap.get("anaemia")),
+                "edd", riskParamsMap.get("edd") != null ? new StringBuilder().append(riskParamsMap.get("edd")),
+                "bp", riskParamsMap.get("bp") != null ? new StringBuilder().append(riskParamsMap.get("bp")),
+                "fhr", riskParamsMap.get("fhr") != null ? new StringBuilder().append(riskParamsMap.get("fhr")),
+                "visitNumber", riskParamsMap.get("visitNumber") != null ? new StringBuilder().append(riskParamsMap.get("visitNumber")),
+                "visitDate", riskParamsMap.get("visitDate") != null ? new StringBuilder().append(riskParamsMap.get("visitDate"))
+        );
+    }
 
-	private Obs getLatestObs(Patient patient, String conceptIdentifier) {
-		Concept concept = Dictionary.getConcept(conceptIdentifier);
-		List<Obs> obs = Context.getObsService().getObservationsByPersonAndConcept(patient, concept);
-		if (obs.size() > 0) {
-			// these are in reverse chronological order
-			return obs.get(0);
-		}
-		return null;
-	}
+    private Obs getLatestObs(Patient patient, String conceptIdentifier) {
+        Concept concept = Dictionary.getConcept(conceptIdentifier);
+        List<Obs> obs = Context.getObsService().getObservationsByPersonAndConcept(patient, concept);
+        if (obs.size() > 0) {
+            // these are in reverse chronological order
+            return obs.get(0);
+        }
+        return null;
+    }
 }
--- omod/src/main/webapp/fragments/patient/patientRisk.gsp
+++ omod/src/main/webapp/fragments/patient/patientRisk.gsp
@@ -2,28 +2,62 @@
     ui.includeCss("kenyaemr", "referenceapplication.css", 100)
 %>
 
+<html>
+<h3>Patient risk report</h3>
+<body>
+<button id="apiButton">Calculate Risk Score</button>
+<div id="apiResponse"></div>
+<script type="text/javascript">
+    document.getElementById("apiButton").addEventListener("click", function() {
+        callMihic();
+    });
+    function callMihic() {
+        const apiUrl = 'http://35.241.202.94:5001/pph_process';
+        const requestData = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "first_name": personName.givenName,
+                "last_name": personName.familyName,
+                "patient_id": currentPatient.id,
+                "data": {
+                    "Pallor  (Y/N/NA)": riskParams.pallor,
+                    "Haemoglobin (Level/ND/NA)": riskParams.haemoglobin,
+                    "Respiratory rate": riskParams.pulse,
+                    "Blood Pressure": riskParams.bp,
+                    "Anaemia": riskParams.anaemia,
+                    "Estimated Date of Child Birth (EDC)": riskParams.edd,
+                    "Date of visit": riskParams.visitDate,
+                    "ANC Number  (Re Visit)": riskParams.visitNumber,
+                    "FHR (bpm)": riskParams.fhr,
+                    "Pallor  (Y/N/NA)_stage": "intrapartum",
+                    "Haemoglobin (Level/ND/NA)_stage": "antepartum",
+                    "Respiratory rate_stage": "intrapartum",
+                    "Blood Pressure_stage": "intrapartum",
+                    "Anaemia_stage": "intrapartum",
+                    "FHR (bpm)_stage": "intrapartum"
+                }
+            }),
+        };
 
+        console.log("Mihic request data" + requestData);
 
-<div>
+        // Make the API POST request
+        fetch(apiUrl, requestData)
+            .then(response => response.json())
+            .then(data => {
+                kenyaui.notifySuccess(data.message);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                kenyaui.notifyError('An error occurred while calculating risk score');
+            });
+    }
 
-
-    <div id="content" class="container">
-
-        <div class="clear"></div>
-        <div class="container">
-            <div class="dashboard clear">
-                <div class="info-container column">
-
-                    <div class="info-section">
-                        <div class="info-header">
-                            <i class="icon-diagnosis"></i>
-                            <h3>Patient risk report</h3>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div>
-<div><a href="https://app.powerbi.com/groups/me/reports/c47f24ee-4c78-4439-9412-0cda3ceb8d96/ReportSection6a634c30e01bad307396?experience=power-bi&filter=unique_patient_id%2Funique_patient_id%20eq%20%27${patientId}%27" target="_blank">View patient risk report</a></div>
-</div>
+</script>
+<div><a href="https://app.powerbi.com/groups/me/reports/c47f24ee-4c78-4439-9412-0cda3ceb8d96/ReportSection6a634c30e01bad307396?experience=power-bi&filter=unique_patient_id%2Funique_patient_id%20eq%20%27${currentPatient.id}%27"
+        target="_blank">View patient risk report</a></div>
+</body>
+</html>
